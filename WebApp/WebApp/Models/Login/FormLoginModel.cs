ad87200ee8aa8716702561d60c84dfdc7c03abb1
keid.someya <keid.someya.ayato@gmail.com>
2025-01-23 17:48:11 +0900

    Login Form Model追加

---
+++ WebApp/WebApp/Models/Login/FormLoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Login
+{
+    public class FormLoginModel
+    {
+        [Required(ErrorMessage = "ログインIDは必須です")]
+        [StringLength(20, ErrorMessage = "ログインIDは20字以内で入力してください")]
+        public string LoginID {  get; set; }
+
+        [Required(ErrorMessage = "パスワードは必須です")]
+        [StringLength(20, ErrorMessage = "パスワードは20字以内で入力してください")]
+        public string Password { get; set; }
+    }
+}
