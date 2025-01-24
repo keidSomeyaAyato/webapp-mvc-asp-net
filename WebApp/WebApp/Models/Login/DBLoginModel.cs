5021b0c3cc727172558bd332b0d3ec6eb20c6ade
keid.someya <keid.someya.ayato@gmail.com>
2025-01-24 14:06:06 +0900

    ,

---
+++ WebApp/WebApp/Models/Login/DBLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Login
+{
+    public class DBLoginModel
+    {
+        public string USER_CD {  get; set; }
+
+        public string USER_NM { get;set; }
+
+        public string PASSWORD {  get; set; }
+    }
+}
