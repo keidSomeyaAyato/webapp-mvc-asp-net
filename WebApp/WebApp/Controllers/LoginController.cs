d27d314b7ea5ad16d46f05d1b7b38f91a9a12eb5
keid.someya <keid.someya.ayato@gmail.com>
2025-01-24 09:41:56 +0900

    一旦POSTのメソッドが動くようになった

--- WebApp/WebApp/Controllers/LoginController.cs
+++ WebApp/WebApp/Controllers/LoginController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models.Login;
 
 namespace WebApp.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
@@ -16,8 +18,9 @@ namespace WebApp.Controllers
 
         // POST : Login
         [HttpPost]
-        public ActionResult Aa()
+        public ActionResult Index(FormLoginModel model)
         {
+            //lll
             return View();
         }
     }
