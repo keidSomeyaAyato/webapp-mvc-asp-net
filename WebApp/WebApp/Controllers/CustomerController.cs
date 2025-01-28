6d4a2c7227fb12869d10893eb779fa01ab37265c
keid.someya <keid.someya.ayato@gmail.com>
2025-01-28 17:05:43 +0900

    TEmp syutoku

--- WebApp/WebApp/Controllers/CustomerController.cs
+++ WebApp/WebApp/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,14 @@ namespace WebApp.Controllers
         // GET: Customer
         public ActionResult List()
         {
+            if (TempData["From"] != null)
+            {
+                string from = TempData["From"].ToString();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
             ViewBag.Title = "タイトルテスト";
             ViewBag.Name = "名前テスト";
             return View("CustomerList");
