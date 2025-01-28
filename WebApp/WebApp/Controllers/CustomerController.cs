39e036dac641f11e99e2ca0485e0a4fafa489922
keid.someya <keid.someya.ayato@gmail.com>
2025-01-28 17:45:13 +0900

    ログイン名表示

--- WebApp/WebApp/Controllers/CustomerController.cs
+++ WebApp/WebApp/Controllers/CustomerController.cs
@@ -4,11 +4,14 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly MST_USERService _userService = new MST_USERService();
+
         // GET: Customer
         public ActionResult List()
         {
@@ -18,6 +21,7 @@ namespace WebApp.Controllers
                 return RedirectToAction("Index", "Login");
             }
 
+            //遷移の確認
             if (TempData["From"] != null)
             {
                 string from = TempData["From"].ToString();
@@ -27,8 +31,11 @@ namespace WebApp.Controllers
             {
                 return RedirectToAction("Index", "Login");
             }
-            ViewBag.Title = "タイトルテスト";
-            ViewBag.Name = "名前テスト";
+
+
+            ViewBag.Title = "顧客一覧";
+            ViewBag.Name = _userService.GetName(Session["USER_CD"].ToString());
+            
             return View("CustomerList");
         }
     }
