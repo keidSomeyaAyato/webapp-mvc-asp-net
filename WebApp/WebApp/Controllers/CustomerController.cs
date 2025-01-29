813ecdd8cc74cc49dbf513d021ed3dc7c2490775
keid.someya <keid.someya.ayato@gmail.com>
2025-01-29 16:05:02 +0900

    初期表示

--- WebApp/WebApp/Controllers/CustomerController.cs
+++ WebApp/WebApp/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor.Tokenizer.Symbols;
 using WebApp.Services;
 
 namespace WebApp.Controllers
@@ -15,6 +16,13 @@ namespace WebApp.Controllers
         // GET: Customer
         public ActionResult List()
         {
+            //vb宣言
+            string vbCondCustomerIdFrom = "";
+            string vbCondCustomerIdTo = "";
+            int vbCustomerType = 4;
+            string vbKeyWord = "";
+            string from;
+
             // ※２．ログイン済みのチェック
             if (Session["USER_CD"] == null)
             {
@@ -24,7 +32,7 @@ namespace WebApp.Controllers
             //遷移の確認
             if (TempData["From"] != null)
             {
-                string from = TempData["From"].ToString();
+                from = TempData["From"].ToString();
                 TempData["From"] = from; //再度登録してリロードできるように
             }
             else
@@ -32,10 +40,31 @@ namespace WebApp.Controllers
                 return RedirectToAction("Index", "Login");
             }
 
-
+            //モード別の処理
+            switch (from)
+            {
+                case "LOGIN":
+                    vbCondCustomerIdFrom = "";
+                    vbCondCustomerIdTo = "";
+                    vbCustomerType = 4;
+                    vbKeyWord = "";
+                    break;
+                case "INPUT":
+                case "CONFIRM":
+                    break;
+                case "DELETE":
+                    break;
+                default:
+                    break;
+            }
+            //Form初期表示
             ViewBag.Title = "顧客一覧";
             ViewBag.Name = _userService.GetName(Session["USER_CD"].ToString());
-            
+            ViewBag.CondCustomerIdFrom = vbCondCustomerIdFrom;
+            ViewBag.CondCustomerIdTo = vbCondCustomerIdTo;
+            ViewBag.CustomerType = vbCustomerType;
+            ViewBag.KeyWord = vbKeyWord;
+
             return View("CustomerList");
         }
     }
