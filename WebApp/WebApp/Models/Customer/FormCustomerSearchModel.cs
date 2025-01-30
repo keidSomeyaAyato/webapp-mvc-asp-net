4b1e4a0417104e50f7b806580e1f65a3696e7ec1
keid.someya <keid.someya.ayato@gmail.com>
2025-01-30 15:00:12 +0900

    form model　作成

---
+++ WebApp/WebApp/Models/Customer/FormCustomerSearchModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Customer
+{
+    public class FormCustomerSearchModel
+    {
+        public string CondCustomerIdFrom { get; set; }
+
+        public string CondCustomerIdTo { get; set; }
+
+        public int chkCustomerType { get; set; }
+
+        public string CondKeyword { get; set; }
+    }
+}
