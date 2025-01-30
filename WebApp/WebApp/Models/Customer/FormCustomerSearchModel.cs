e3d783f105c3e0d78de913926df0753a0f4f6162
keid.someya <keid.someya.ayato@gmail.com>
2025-01-30 15:18:45 +0900

    Update FormCustomerSearchModel.cs

--- WebApp/WebApp/Models/Customer/FormCustomerSearchModel.cs
+++ WebApp/WebApp/Models/Customer/FormCustomerSearchModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace WebApp.Models.Customer
 {
     public class FormCustomerSearchModel
     {
-        public string CondCustomerIdFrom { get; set; }
+        [Range(0, 99999999, ErrorMessage = "8桁以内の数値を入力してください。")]
+        public int CondCustomerIdFrom { get; set; }
 
-        public string CondCustomerIdTo { get; set; }
+        [Range(0, 99999999, ErrorMessage = "8桁以内の数値を入力してください。")]
+        public int CondCustomerIdTo { get; set; }
 
         public int chkCustomerType { get; set; }
 
