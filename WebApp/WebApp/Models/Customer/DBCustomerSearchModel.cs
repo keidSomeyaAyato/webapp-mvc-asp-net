418e5c922d942bd9707421002bb273ed7ea4bcc1
keid.someya <keid.someya.ayato@gmail.com>
2025-01-31 16:27:46 +0900

    モデル作成

--- WebApp/WebApp/Models/Customer/DBCustomerSearchModel.cs
+++ WebApp/WebApp/Models/Customer/DBCustomerSearchModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace WebApp.Models.Customer
 {
+    [Table("MST_CUSTOMER")]
     public class DBCustomerSearchModel
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
 
+        public string Type { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    public class MVCDBContext : DbContext
+    {
     }
 }
