1dd698351e14565bedcec4ee46e1e1c871f8a665
keid.someya <keid.someya.ayato@gmail.com>
2025-01-31 17:28:27 +0900

    service add

---
+++ WebApp/WebApp/Services/CustomerSearchService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models.Customer;
+
+namespace WebApp.Services
+{
+    public class CustomerSearchService
+    {
+        private readonly MVCDBContext _dbContext;
+
+        public CustomerSearchService()
+        {
+            _dbContext = new MVCDBContext();
+        }
+
+        public List<DBCustomerSearchModel> GetCustomerFirstToTen()
+        {
+            string sql = "SELECT TOP 10 * FROM MST_CUSTOMER ORDER BY CUST_ID ASC";
+
+            return _dbContext.Database.SqlQuery<DBCustomerSearchModel>(sql).ToList();
+        }
+    }
+}
